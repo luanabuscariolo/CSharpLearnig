928ec1bf8faf4bc908cca8c9e5ac3b7c867a1d80
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2024-01-26 09:15:49 +0000

    Update

---
+++ ConstrutoresDestrutores/ConstrutoresDestrutores/Carro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutoresDestrutores
+{
+    internal class Carro
+    {
+        public string Modelo { get; set; }
+        public int Portas { get; set; }
+        public double Preco { get; set; }
+
+        public Carro() // Construtor Padrão
+        {
+            Console.WriteLine("Um novo objeto carro foi criado.");
+        }
+        public Carro(string modelo) : this() // Construtor Parametrizado
+        {
+            this.Modelo = modelo;
+        }
+    }
+}
---
+++ ConstrutoresDestrutores/ConstrutoresDestrutores/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConstrutoresDestrutores
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+            c1.Modelo = "Fusca";
+            c1.Preco = 9700;
+            c1.Portas = 2;
+
+            Carro c2 = new Carro();
+            c2.Modelo = "Opala";
+            c2.Preco = 49500;
+            c2.Portas = 4;
+
+            Carro c3 = new Carro("Palio"); 
+        }
+    }
+}
