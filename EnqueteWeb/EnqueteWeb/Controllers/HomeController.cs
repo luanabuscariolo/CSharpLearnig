e6fcf0063425996d280b528487b346cf7f40db60
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2024-02-07 16:27:43 +0000

    Update EnqueteWeb

--- EnqueteWeb/EnqueteWeb/Controllers/HomeController.cs
+++ EnqueteWeb/EnqueteWeb/Controllers/HomeController.cs
@@ -6,27 +6,14 @@ namespace EnqueteWeb.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult Responder()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
