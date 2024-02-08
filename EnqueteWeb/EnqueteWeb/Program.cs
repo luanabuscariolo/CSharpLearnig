b5edd76215c4743b3ce2467d2a5148dd51f59fcb
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2024-02-08 19:44:48 +0000

    Update Program.cs

--- EnqueteWeb/EnqueteWeb/Program.cs
+++ EnqueteWeb/EnqueteWeb/Program.cs
@@ -5,7 +5,7 @@ internal class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        builder.Services.AddControllersWithViews(); //.AddRazorRuntimeCompilation();
+        builder.Services.AddControllersWithViews(); //.AddRazorRuntimeCompilation(); //PARA VS CODE PODE SER ÚTIL
 
         var app = builder.Build();
 
