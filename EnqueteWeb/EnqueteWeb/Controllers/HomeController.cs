9b69d1398c92f464903e8c4c6e8cfaf28d80043f
Luana Buscariolo <67836162+luanabuscariolo@users.noreply.github.com>
2024-02-08 17:12:40 +0000

    Update projeto EnqueteWeb

--- EnqueteWeb/EnqueteWeb/Controllers/HomeController.cs
+++ EnqueteWeb/EnqueteWeb/Controllers/HomeController.cs
@@ -15,5 +15,23 @@ namespace EnqueteWeb.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Responder(Resposta resposta)
+        {
+            if (ModelState.IsValid)
+            {
+                Repositorio.AdicionarResposta(resposta);
+                return View("Obrigado");
+            }
+            else
+            {
+                return View(resposta);
+            }
+        }
+
+        public IActionResult Resultado()
+        {
+            return View(Repositorio.Respostas);
+        }
     }
 }
---
+++ EnqueteWeb/EnqueteWeb/Models/Repositorio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+namespace EnqueteWeb.Models
+{
+    public class Repositorio
+    {
+        private static List<Resposta> respostas = new List<Resposta>();
+        public static IEnumerable<Resposta> Respostas { get { return respostas; } }
+
+        public static void AdicionarResposta(Resposta resposta)
+        {
+            respostas.Add(resposta);
+        }
+
+        static Repositorio()
+        {
+            respostas.Add(new Resposta() { Nome = "Vitor", Email = "vitor@gmail.com", Sim = true });
+            respostas.Add(new Resposta() { Nome = "Maria", Email = "maria@gmail.com", Sim = true });
+            respostas.Add(new Resposta() { Nome = "José", Email = "jose@gmail.com", Sim = false });
+            respostas.Add(new Resposta() { Nome = "Paulo", Email = "paulo@gmail.com", Sim = true });
+            respostas.Add(new Resposta() { Nome = "Nazaré", Email = "nazare@gmail.com", Sim = false });
+        }
+    }
+}
